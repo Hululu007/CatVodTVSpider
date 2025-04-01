a947f0a9d90b3f23691ee89ef23b1011a68cdb69
Hululu007 <Hululu007@users.noreply.github.com>
2025-04-01 19:17:13 +0000

    update files

--- app/src/main/java/com/github/catvod/spider/TianYi.java
+++ app/src/main/java/com/github/catvod/spider/TianYi.java
@@ -2,10 +2,9 @@ package com.github.catvod.spider;
 
 import android.content.Context;
 import android.text.TextUtils;
-
-import com.github.catvod.api.UCApi;
+import com.github.catvod.api.TianyiApi;
 import com.github.catvod.bean.Result;
-import com.github.catvod.bean.uc.ShareData;
+import com.github.catvod.bean.tianyi.ShareData;
 import com.github.catvod.crawler.Spider;
 import com.github.catvod.crawler.SpiderDebug;
 
@@ -15,52 +14,51 @@ import java.util.Locale;
 import java.util.Map;
 
 /**
- * @author ColaMint & Adam & FongMi
+ * @author lushunming
  */
-public class UC extends Spider {
+public class TianYi extends Spider {
 
 
     @Override
     public void init(Context context, String extend) throws Exception {
 
-        UCApi.get().setCookie(extend);
+        TianyiApi.get().setCookie(extend);
     }
 
     @Override
     public String detailContent(List<String> ids) throws Exception {
 
-        ShareData shareData = UCApi.get().getShareData(ids.get(0));
-        return Result.string(UCApi.get().getVod(shareData));
+        ShareData shareData = TianyiApi.get().getShareData(ids.get(0), "");
+        return Result.string(TianyiApi.get().getVod(shareData));
     }
 
 
     @Override
     public String playerContent(String flag, String id, List<String> vipFlags) throws Exception {
-        String playContent = UCApi.get().playerContent(id.split("\\+\\+"), flag);
-        SpiderDebug.log("playContent:" + playContent);
-        return playContent;
+        return TianyiApi.get().playerContent(id.split("\\+\\+"), flag);
+
     }
 
     /**
      * 獲取詳情內容視頻播放來源（多 shared_link）
      *
      * @param ids share_link 集合
-     * @param i
+     * @param
      * @return 詳情內容視頻播放來源
      */
     public String detailContentVodPlayFrom(List<String> ids, int index) {
         List<String> playFrom = new ArrayList<>();
        /* if (ids.size() < 2){
-            return TextUtils.join("$$$",  UCApi.get().getPlayFormatList());
+            return TextUtils.join("$$$",  TianyiApi.get().getPlayFormatList());
         }*/
 
         for (int i = 1; i <= ids.size(); i++) {
 
-            for (String s : UCApi.get().getPlayFormatList()) {
-                playFrom.add(String.format(Locale.getDefault(), "uc" + s + "#%02d%02d", i, index));
+            for (String s : TianyiApi.get().getPlayFormatList()) {
+                playFrom.add(String.format(Locale.getDefault(), "天意" + s + "#%02d%02d", i, index));
 
             }
-            playFrom.add("uc原画" + i + index);
+            playFrom.add("天意" + i + index);
         }
         return TextUtils.join("$$$", playFrom);
     }
@@ -74,16 +72,15 @@ public class UC extends Spider {
     public String detailContentVodPlayUrl(List<String> ids) throws Exception {
         List<String> playUrl = new ArrayList<>();
         for (String id : ids) {
-            ShareData shareData = UCApi.get().getShareData(id);
-            playUrl.add(UCApi.get().getVod(shareData).getVodPlayUrl());
+            ShareData shareData = TianyiApi.get().getShareData(id, "");
+            try {
+                playUrl.add(TianyiApi.get().getVod(shareData).getVodPlayUrl());
+            } catch (Exception e) {
+                SpiderDebug.log("获取播放地址出错:" + e.getMessage());
+            }
         }
         return TextUtils.join("$$$", playUrl);
     }
 
-    public static Object[] proxy(Map<String, String> params) throws Exception {
-        String type = params.get("type");
-        if ("video".equals(type)) return UCApi.get().proxyVideo(params);
-        //if ("sub".equals(type)) return AliYun.get().proxySub(params);
-        return null;
-    }
+
 }
